08fc1cdd23069d74a4a63fccf20305cd2cb55a7a
Cameron Smith <Cs11@usf.edu>
2024-08-30 13:51:13 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,12 @@
+// Task 1: Variables and Data Types
+
+let customerName = "Nikita Kucherov";
+const customerID =818;
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
+
+
+
